5a6d848d6204728b2adf57b78da701a80bb8f930
KDev <bunmak.got@gmail.com>
2020-07-06 14:56:41 +0700

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'spv-ng-frontend';
+  test = { a: 'hello', b: 'world' };
+  c = 0;
+  onClick() {
+    this.c++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Com1Component } from './components/com1/com1.component';
+import { Com2Component } from './components/com2/com2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Com1Component,
+    Com2Component
   ],
   imports: [
     BrowserModule,
